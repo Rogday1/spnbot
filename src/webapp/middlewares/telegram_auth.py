06e05aa2f238f8b7410730393a2eb6e962cc5549
jki8930 <ruslanisakov656@gmail.com>
2025-06-14 22:25:23 +0400

    SHA241

--- src/webapp/middlewares/telegram_auth.py
+++ src/webapp/middlewares/telegram_auth.py
@@ -396,25 +396,25 @@ class TelegramAuthMiddleware(BaseHTTPMiddleware):
                 params_list = []
                 exclude_params = ['hash', 'signature']
                 
-                # Разбиваем декодированные данные на параметры
-                decoded_data = unquote(init_data_raw)
-                for param in decoded_data.split('&'):
+                # Разбиваем оригинальные данные на параметры
+                for param in init_data_raw.split('&'):
                     if not param:
                         continue
                     if '=' in param:
                         key, value = param.split('=', 1)
                         if key in exclude_params:
                             continue
-                        # Сохраняем параметр в декодированном виде (key=value)
-                        params_list.append(f"{key}={value}")
+                        # Сохраняем параметр в оригинальном виде (key=value)
+                        params_list.append(param)
                     else:
                         params_list.append(param)
                 
                 # Сортируем параметры по ключу (лексикографически)
-                params_list.sort(key=lambda p: p.split('=', 1)[0])
+                params_list.sort()
                 
-                # Объединяем через символ новой строки
+                # Объединяем через символ новой строки (без дополнительного кодирования)
                 data_check_string = '\n'.join(params_list)
+                logging.info(f"Проверочная строка перед хешированием: {data_check_string}")
                 
                 # Логируем параметры и проверочную строку для диагностики
                 logging.info(f"Параметры для проверки: {params_list}")
